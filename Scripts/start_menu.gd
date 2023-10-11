037da90121d2be279cf5229a44d78b89adc14833
Victor Turgeon <masfervic@hotmail.com>
2023-10-11 11:45:08 -0400

    balance and main screen ambiance

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$backgroundAmbiance.play(0.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
