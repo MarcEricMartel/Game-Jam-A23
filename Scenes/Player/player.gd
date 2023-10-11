9d308b53705c5371dcf59e38a04b1970633f94b4
Victor Turgeon <masfervic@hotmail.com>
2023-10-10 20:37:31 -0400

    fullscreen controls

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -6,10 +6,19 @@ func _ready():
 	pass
 	
 func _process(delta):
+	handle_menu_inputs()
+	
 	if Input.is_action_just_pressed("place_spawnable"):
-		spawnBat()
+		spawn_bat()
 
-func spawnBat():
+func handle_menu_inputs():
+	if Input.is_action_just_pressed("fullscreen_toggle"):
+		if get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
+			get_window().mode = Window.MODE_WINDOWED
+		else:
+			get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN
+
+func spawn_bat():
 	var bat = batTemplatePath.instantiate()
 	bat.global_position = get_global_mouse_position()
 	$"..".add_child(bat)
